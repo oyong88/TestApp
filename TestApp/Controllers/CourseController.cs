f30ceaf0bccc3c06b89ebd166298192936883fe8
oyong88 <oyong88@outlook.com>
2022-01-16 16:12:14 +0900

    초기 커밋.

---
+++ TestApp/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.Models;
+using TestApp.Services;
+
+namespace TestApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _config;
+
+        public CourseController(CourseService _svc, IConfiguration config)
+        {
+            _course_service = _svc;
+            _config = config;
+        }
+
+        // The Index method is used to get a list of courses and return it to the view
+        public IActionResult Index()
+        {
+            IEnumerable<Course> _course_list = _course_service.GetCourses(_config.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+    }
+}
---
+++ TestApp/Services/CourseService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.Services
+{
+    public class CourseService
+    {
+        private SqlConnection GetConnection(string conn)
+        {
+            return new SqlConnection(conn);
+        }
+
+        public IEnumerable<Course> GetCourses(string conn)
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "SELECT CourseID,CourseName,Rating from Course";
+            SqlConnection _connection = GetConnection(conn);
+            // Let's open the connection
+            _connection.Open();
+            // We then construct the statement of getting the data from the Course table
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            // Using the SqlDataReader class , we will read all the data from the Course table
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseID = _reader.GetInt32(0),
+                        CourseName = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+                    };
+
+                    _lst.Add(_course);
+                }
+            }
+            _connection.Close();
+            return _lst;
+        }
+
+    }
+}
